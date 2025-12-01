e33168a726da7731cd5b99130318a9a10d262f47
nobiehl <n.biehl@gmx.de>
2025-12-01 23:16:28 +0100

    chore: bump app version to 1.12.3

--- markdown-viewer/MarkdownViewer/MainForm.cs
+++ markdown-viewer/MarkdownViewer/MainForm.cs
@@ -26,7 +26,7 @@ namespace MarkdownViewer
     /// </summary>
     public class MainForm : Form, IMainView
     {
-        private const string Version = "1.12.2";
+        private const string Version = "1.12.3";
 
         // UI Components
         private WebView2 _webView = null!;
